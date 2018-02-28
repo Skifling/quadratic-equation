65ce84c911c4b33df8fb64d7742f474aa652d88d
Aleksei Labkovich <31310602+Skifling@users.noreply.github.com>
2018-02-28 13:15:13 +0300

    Update index.js
    Failed to check task - попытка исправить

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 module.exports = solveEquation(equation) {
   // your implementation
   
-	arr = equation.split(' ') ;   
+	var arr = equation.split(' ') ;   
     if (arr.length == 9) { 
-         a = parseFloat(arr[0]);
-         b = parseFloat(arr[4]);
-         c = parseFloat(arr[8]);
-         s1 = arr[3];
-         s2 = arr[7];
+         var a = parseFloat(arr[0]);
+         var b = parseFloat(arr[4]);
+         var c = parseFloat(arr[8]);
+         var s1 = arr[3];
+         var s2 = arr[7];
     }
     if (arr.length == 5) {
-        a = arr[0];
-        b = 0;
-        c = arr[4];
-        s1 = '+';
-        s2 = arr[3];
+        var a = arr[0];
+        var b = 0;
+        var c = arr[4];
+        var s1 = '+';
+        var s2 = arr[3];
     }
     if (s2 == '-'){
         c = - c;
@@ -24,10 +24,10 @@ module.exports = solveEquation(equation) {
     }
     
     
-        d = parseFloat(Number(b) * Number(b) - 4 * Number(a) * Number(c));
+        var d = parseFloat(Number(b) * Number(b) - 4 * Number(a) * Number(c));
 
-        x1 = Math.round((- b + Math.sqrt(d))/ (2 * a));
-        x2 = Math.round((- b - Math.sqrt(d))/ (2 * a));
+        var x1 = Math.round((- b + Math.sqrt(d))/ (2 * a));
+        var x2 = Math.round((- b - Math.sqrt(d))/ (2 * a));
 	if (x1 < x2) {
         var x = [x1,x2];
 	} else {
