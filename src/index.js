8f9ccd13203d775f02d0e6be7b1816f192815b71
Aleksei Labkovich <31310602+Skifling@users.noreply.github.com>
2018-02-28 12:47:31 +0300

    Update index.js
    final

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = solveEquation(equation) {
   // your implementation
   
- arr = equation.split(' ') ;   
+	arr = equation.split(' ') ;   
     if (arr.length == 9) { 
-         a = arr[0];
-         b = arr[4];
-         c = arr[8];
+         a = parseFloat(arr[0]);
+         b = parseFloat(arr[4]);
+         c = parseFloat(arr[8]);
          s1 = arr[3];
          s2 = arr[7];
     }
@@ -16,23 +16,23 @@ module.exports = solveEquation(equation) {
         s1 = '+';
         s2 = arr[3];
     }
-    
- 
-    if (s2 == '+') {
-        d = Number(b) * Number(b) - 4 * Number(a) * Number(c);
-    }
-    if (s2 == '-') {
-        d = Number(b) * Number(b) + 4 * Number(a) * Number(c); 
-    }
-    if (s1 == '+') {
-        x1 = (- b + Math.sqrt(d))/ (2 * a);
-        x2 = (- b - Math.sqrt(d))/ (2 * a);
+    if (s2 == '-'){
+        c = - c;
     }
     if (s1 == '-') {
-        x1 = (- b + Math.sqrt(d))/ (2 * a);
-        x2 = (- b - Math.sqrt(d))/ (2 * a);
+        b = - b;
     }
+    
+    
+        d = parseFloat(Number(b) * Number(b) - 4 * Number(a) * Number(c));
+
+        x1 = Math.round((- b + Math.sqrt(d))/ (2 * a));
+        x2 = Math.round((- b - Math.sqrt(d))/ (2 * a));
+	if (x1 < x2) {
+        var x = [x1,x2];
+	} else {
+		var x = [x2,x1];
+	}
  
- 
- return a + ',' + b + ',' + c + ',' + s1 + ',' + s2 + ',' + d + ',' + arr.length + ',' + x1 + ',' + x2;
+	return x;
 }
